c0e1da68ebba9630f949f639f3c7a9be70340773
Isaac Ferreira Filho <isaacmob@riseup.net>
2018-06-25 18:01:56 -0300

    Criado o Olá Mundo.

--- afluente/core/views.py
+++ afluente/core/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(requests):
-    return HttpResponse()
+    return HttpResponse('Olá Mundo!')
--- afluente/tests/tests.py
+++ afluente/tests/tests.py
@@ -1,3 +1,10 @@
+from afluente.django_assertions import dj_assert_contains
+
+
 def test_status_code(client):
     response = client.get('/')
     assert 200 == response.status_code
+
+def test_home(client):
+    response = client.get('/')
+    dj_assert_contains(response, 'Olá Mundo!')
