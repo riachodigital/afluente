f51ac4814b8836797f6c118f1ea04a0d2cb6cd2e
Isaac Ferreira Filho <isaacmob@riseup.net>
2018-06-25 19:14:43 -0300

    Consertado código de acordo com o flake8

--- afluente/tests/tests.py
+++ afluente/tests/tests.py
@@ -5,6 +5,7 @@ def test_status_code(client):
     response = client.get('/')
     assert 200 == response.status_code
 
+
 def test_home(client):
     response = client.get('/')
     dj_assert_contains(response, 'Olá Mundo!')
