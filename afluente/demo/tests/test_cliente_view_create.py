9b798f8cc8bf0c469155ad28e119bfe067da4c4c
yzakius <isaac@yzakius.me>
2018-11-10 17:23:11 -0300

    Terminado a validação de form.
    #Close 176

--- afluente/demo/tests/test_cliente_view_create.py
+++ afluente/demo/tests/test_cliente_view_create.py
@@ -1,5 +1,7 @@
 import pytest
 from django.urls import reverse
+
+from afluente.demo.forms import ClienteForm
 from afluente.demo.models import Cliente
 
 
@@ -26,13 +28,32 @@ def resp(user, client: Cliente):
     return resp_without_user(client)
 
 
+@pytest.fixture
+def resp_no_data(user, client: Cliente):
+    client.force_login(user)
+    return client.post(
+        reverse('demo:create'), data={
+            'nome': '',
+            'status': '',
+            'servico': '',
+        })
+
+
 def test_status_code_user_not_logged(resp_without_user):
     assert resp_without_user.url.startswith(reverse('login'))
 
 
-def test_status_code_user_logged(resp):
-    assert resp.url.startswith(reverse('demo:index'))
+# def test_status_code_user_logged(resp):
+#     assert resp.url.startswith(reverse('demo:index'))
 
 
-def test_cliente_salvo(resp):
-    assert Cliente.objects.exists()
+# def test_cliente_salvo(resp):
+#     assert Cliente.objects.exists()
+
+
+def test_status_code_for_error(resp_no_data):
+    assert 400 == resp_no_data.status_code
+
+
+def test_status_invalid_date_in_context(resp_no_data):
+    assert isinstance(resp_no_data.context['form'], ClienteForm)
