0742002347d8af26cb933af3f6f40993a4b30c99
Gustavo Sousa <ola@gustavosousa.me>
2023-06-29 23:24:17 -0300

    Blue code formatter installed

--- afluente/customers/tests/test_models.py
+++ afluente/customers/tests/test_models.py
@@ -3,9 +3,9 @@ from afluente.customers.models import Customer
 
 def test_if_customer_returns_name(db):
     customer = Customer.objects.create(
-        name="Customer Inc", 
-        responsible="Responsible", 
-        email="test@customer.com", 
-        phone="819999999"
-        )
+        name='Customer Inc',
+        responsible='Responsible',
+        email='test@customer.com',
+        phone='819999999',
+    )
     assert customer.name == str(customer)
--- afluente/services/facade.py
+++ afluente/services/facade.py
@@ -7,10 +7,18 @@ from afluente.services.models import Service
 
 def list_services_that_expire_this_month():
     month = timezone.now()
-    return list(Service.objects.filter(end_date__month=month.month, end_date__year=month.year))
+    return list(
+        Service.objects.filter(
+            end_date__month=month.month, end_date__year=month.year
+        )
+    )
 
 
 def list_services_that_expire_next_month():
     month = date.today()
     next_month = month + timedelta(days=30)
-    return list(Service.objects.filter(end_date__month=next_month.month, end_date__year=month.year))
+    return list(
+        Service.objects.filter(
+            end_date__month=next_month.month, end_date__year=month.year
+        )
+    )
